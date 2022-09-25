1262951328972172b4eb4194bc427384af2a6847
Cfmunoz8 <cfmunoz8@gmail.com>
2022-09-25 15:47:20 +0000

    2do commit

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -5,29 +5,40 @@ function App() {
   const [task, setTask] = useState("");
   const [list, setList] = useState([]);
   return (
-    <div className="container container-fluid bg-light">
-      <div className="">
+    <div
+      className="container container-fluid bg-light pb-3 mt-3"
+      style={{ width: "560px" }}
+    >
+      <div className="text-center">
         <h1>Mi lista de tareas</h1>
         <h2>¿Qué me falta por hacer?</h2>
       </div>
       <div>
         <div className="card" style={{ width: "530px" }}>
-          <div className="card-header">
+          <div className="card-header row">
             <input
+              className="col-8"
               type="text"
-              style={{ width: "500px" }}
+              style={{ width: "350px" }}
               placeholder="Escribe aquí tu tarea pendiente"
-              onKeyDown={(event) => setTask(event.target.value)}
-              onKeyUp={(sameEvent) => setList(list.concat(sameEvent))}
+              onChange={(event) => setTask(event.target.value)}
             ></input>
+            <button
+              className="btn btn-success"
+              style={{ width: "150px" }}
+              onClick={() => {
+                setList(list.concat(`${task}`));
+              }}
+            >
+              Agregar Tarea
+            </button>
           </div>
           <ul className="list-group list-group-flush">
-            {list.map((task) => (
-              <li className="list-group-item">{task}</li>
+            {list.map((tarea) => (
+              <li className="list-group-item">{tarea}</li>
             ))}
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
           </ul>
+          <div class="card-footer">Tareas pendientes: {list.length}</div>
         </div>
       </div>
     </div>
